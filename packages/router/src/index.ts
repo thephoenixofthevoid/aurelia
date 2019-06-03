4873ed8731ea966ba9490830f43d0f61e926da60
Jürgen Wenzel <jurgen@jwenzel.se>
2019-06-04 01:44:33 +0200

    feat(router): remove queued browser history

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -2,7 +2,6 @@ export * from './browser-navigation';
 export * from './link-handler';
 export * from './nav';
 export * from './navigator';
-export * from './queued-browser-history';
 export * from './route-recognizer';
 export * from './router';
 export * from './scope';
